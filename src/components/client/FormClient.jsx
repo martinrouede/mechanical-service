5194cb299dbce525449e2350aacd080a75677a27
Martin Rouede <martinrouede1@gmail.com>
2022-02-07 21:40:08 -0300

    se acomodan componentes

--- src/components/client/FormClient.jsx
+++ src/components/client/FormClient.jsx
@@ -1,104 +1,34 @@
-import { useState, useEffect } from "react";
-import { useNavigate, useParams } from "react-router-dom";
-import axios from 'axios';
+import { Form } from 'react-bootstrap';
 
-import { Button, Form } from 'react-bootstrap';
+const FormClient = props => (
+    <Form>
+        <Form.Group className="mb-3" controlId="formBasicCarModel">
+            <Form.Label>Marca Auto</Form.Label>
+            <Form.Control
+                type="text"
+                onChange={(e) => props.setCarModel(e.target.value)}
+                value={props.carModel}
+            />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCarPatent">
+            <Form.Label>Patente</Form.Label>
+            <Form.Control
+                type="text"
+                onChange={(e) => props.setCarPatent(e.target.value)}
+                value={props.carPatent}
+            />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCarKilometers">
+            <Form.Label>Kilometraje</Form.Label>
+            <Form.Control
+                type="number"
+                onChange={(e) => props.setCarKilometers(e.target.value)}
+                value={props.carKilometers}
+                onWheel={(e) => e.target.blur()}
+            />
+        </Form.Group>
+    </Form>
 
-function NewClient(props) {
-    const navigate = useNavigate();
-    const params = useParams();
+);
 
-    const [clientName, setClientName] = useState('');
-    const [carModel, setCarModel] = useState('');
-    const [carPatent, setCarPatent] = useState('');
-    const [carKilometers, setCarKilometers] = useState(0);
-
-    useEffect(() => {
-        if (params.id) {
-            axios.get(`http://localhost:3000/customers/${params.id}`).then((response) => {
-                const client = response.data;
-                setClientName(client.name);
-                setCarModel(client.carModel);
-                setCarPatent(client.carPatent);
-                setCarKilometers(client.carKilometers);
-            });
-        }
-    }, [])
-
-
-    const submit = () => {
-        if (params.id) {
-            axios.put(`http://localhost:3000/customers/${params.id}`, {
-                name: clientName,
-                carModel: carModel,
-                carPatent: carPatent,
-                carKilometers: Number(carKilometers)
-            }).then((response) => {
-                console.log(response);
-            });
-        }
-        else {
-            axios.post(`http://localhost:3000/customers/`, {
-                name: clientName,
-                carModel: carModel,
-                carPatent: carPatent,
-                carKilometers: Number(carKilometers)
-            }).then((response) => {
-                console.log(response);
-            });
-        }
-    }
-
-    const goBack = () => {
-        navigate(`/cliente/lista/`);
-    }
-
-    return (
-        <>
-            <Form>
-                <Form.Group className="mb-3" controlId="formBasicClient">
-                    <Form.Label>Cliente</Form.Label>
-                    <Form.Control
-                        type="text"
-                        onChange={(e) => setClientName(e.target.value)}
-                        value={clientName}
-                    />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCarModel">
-                    <Form.Label>Marca Auto</Form.Label>
-                    <Form.Control
-                        type="text"
-                        onChange={(e) => setCarModel(e.target.value)}
-                        value={carModel}
-                    />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCarPatent">
-                    <Form.Label>Patente</Form.Label>
-                    <Form.Control
-                        type="text"
-                        onChange={(e) => setCarPatent(e.target.value)}
-                        value={carPatent}
-                    />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCarKilometers">
-                    <Form.Label>Kilometraje</Form.Label>
-                    <Form.Control
-                        type="number"
-                        onChange={(e) => setCarKilometers(e.target.value)}
-                        value={carKilometers}
-                    />
-                </Form.Group>
-            </Form>
-            <div className="button-container">
-                <Button variant="outline-primary" onClick={goBack}>
-                    Volver
-                </Button>
-                <Button variant="primary" onClick={submit}>
-                    Guardar
-                </Button>
-            </div>
-        </>
-    );
-}
-
-export default NewClient;
+export default FormClient;
--- src/components/service/FormService.jsx
+++ src/components/service/FormService.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from 'react';
 import { useNavigate, useParams } from "react-router-dom";
 import axios from 'axios';
 import { dateFormat, newDate } from '../../services/date';
+import FormClient from '../client/FormClient';
+import { filterDuplicates } from '../../services/utils';
 
 import { Button, Form } from 'react-bootstrap';
 
@@ -11,82 +13,105 @@ import "react-datepicker/dist/react-datepicker.css";
 import { Typeahead } from 'react-bootstrap-typeahead';
 import 'react-bootstrap-typeahead/css/Typeahead.css';
 
-function NewService(props) {
+const NewService = (props) => {
     const navigate = useNavigate();
     const params = useParams();
 
-    const [date, setDate] = useState(newDate());
     const [client, setClient] = useState([]);
+    const [clientNameNoEdit, setClientNameNoEdit] = useState('');
+
+
+    const [date, setDate] = useState(newDate());
+    const [clientName, setClientName] = useState('');
+    const [carModel, setCarModel] = useState('');
+    const [carPatent, setCarPatent] = useState('');
+    const [carKilometers, setCarKilometers] = useState(0);
     const [repair, setRepair] = useState('');
     const [amount, setAmount] = useState(0);
-    const [carKilometersUpdated, setCarKilometersUpdated] = useState(0);
-
-    let carKilometers;
 
     const [customers, setCustomers] = useState([]);
 
     useEffect(() => {
-        axios.get(`http://localhost:3000/customers`).then((response) => {
-            setCustomers(response.data);
-        });
-        if (params.id) {
-            axios.get(`http://localhost:3000/services/${params.id}?_expand=customers`).then((response) => {
+        axios.get(`http://localhost:3000/services`).then((response) => {
+            setCustomers(filterDuplicates(response.data));
+        }); if (params.id) {
+            axios.get(`http://localhost:3000/services/${params.id}`).then((response) => {
                 const service = response.data;
                 setDate(newDate(service.date));
-                setClient(service.customers);
+                setClientName(service.clientName);
+                setClientNameNoEdit(service.clientName);
+                setCarModel(service.carModel);
+                setCarPatent(service.carPatent);
+                setCarKilometers(service.carKilometers);
                 setRepair(service.repair);
                 setAmount(service.amount);
-                carKilometers = service.customers.carKilometers;
-                setCarKilometersUpdated(service.customers.carKilometers);
             });
         }
     }, [])
 
-    const removeClientData = () => {
-        setClient({ carModel: "", carPatent: "", carKilometers: 0 });
-        setCarKilometersUpdated(0);
+    const removeClientData = (clientData) => {
+        setClient([]);
+        setClientName(clientData);
+        setCarModel("");
+        setCarPatent("");
+        setCarKilometers(0);
     }
 
     const completeClientData = (clientData) => {
         const data = clientData[0];
         if (data) {
             setClient(data);
-            setCarKilometersUpdated(data.carKilometers);
+            setCarModel(data.carModel);
+            setCarPatent(data.carPatent);
+            setCarKilometers(data.carKilometers);
         }
     }
 
     const submit = () => {
-        if (carKilometers !== carKilometersUpdated) {
-            axios.put(`http://localhost:3000/customers/${client.id}`, {
-                name: client.name,
-                carModel: client.carModel,
-                carPatent: client.carPatent,
-                carKilometers: Number(carKilometersUpdated)
-            }).then((response) => {
-                console.log(response);
-            });
+        const payload = {
+            date: dateFormat(date, 'YYYY-MM-DD'),
+            clientName: client.id ? client.clientName.toUpperCase() : clientName.toUpperCase(),
+            carModel: carModel.toUpperCase(),
+            carKilometers: Number(carKilometers),
+            carPatent: carPatent.toUpperCase(),
+            repair: repair.toUpperCase(),
+            amount: Number(amount)
         }
-
         if (params.id) {
-            axios.put(`http://localhost:3000/services/${params.id}`, {
-                customersId: client.id,
-                date: newDate(date),
-                repair: repair,
-                amount: Number(amount)
-            }).then((response) => {
-                console.log(response);
-            });
+            if (clientNameNoEdit !== payload.clientName) {
+                axios.get(`http://localhost:3000/services?clientName=${clientNameNoEdit}`).then((response) => {
+                    response.data.forEach(e => {
+                        if (Number(params.id) === e.id) {
+                            axios.put(`http://localhost:3000/services/${params.id}`, payload).then((response) => {
+                                console.log(response);
+                            });
+                        }
+                        else {
+                            axios.put(`http://localhost:3000/services/${e.id}`, { ...e, clientName: payload.clientName }).then((response) => {
+                                console.log(response);
+                            });
+                        }
+                    })
+                });
+            }
+            else {
+                axios.put(`http://localhost:3000/services/${params.id}`, payload).then((response) => {
+                    console.log(response);
+                });
+            }
+
         }
         else {
-            axios.post(`http://localhost:3000/services`, {
-                customersId: client.id,
-                date: newDate(date),
-                repair: repair,
-                amount: Number(amount)
-            }).then((response) => {
+            axios.post(`http://localhost:3000/services`, payload).then((response) => {
                 console.log(response);
             });
         }
+    }
+
+    const remove = (id) => {
+        axios.delete(`http://localhost:3000/services/${id}`).then((response) => {
+            console.log(response);
+        });
     }
 
     const goBack = () => {
@@ -106,40 +131,31 @@ function NewService(props) {
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicClient">
                     <Form.Label>Cliente</Form.Label>
-                    <Typeahead
-                        id="basic-example"
-                        emptyLabel="Sin resultados"
-                        labelKey={option => `${option.name}`}
-                        placeholder={client.name}
-                        onInputChange={removeClientData}
-                        onChange={(e) => completeClientData(e)}
-                        options={customers}
-                    />
+                    {params.id ?
+                        <Form.Control
+                            type="text"
+                            onChange={(e) => setClientName(e.target.value)}
+                            value={clientName}
+                        />
+                        : <Typeahead
+                            id="basic-example"
+                            emptyLabel="Sin resultados"
+                            labelKey={option => `${option.clientName}`}
+                            placeholder={client.clientName}
+                            onInputChange={(e) => removeClientData(e)}
+                            onChange={(e) => completeClientData(e)}
+                            options={customers}
+                        />
+                    }
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCarModel">
-                    <Form.Label>Marca Auto</Form.Label>
-                    <Form.Control
-                        type="text"
-                        disabled
-                        value={client.carModel}
-                    />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCarPatent">
-                    <Form.Label>Patente</Form.Label>
-                    <Form.Control
-                        type="text"
-                        disabled
-                        value={client.carPatent}
-                    />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCarKilometers">
-                    <Form.Label>Kilometraje</Form.Label>
-                    <Form.Control
-                        type="number"
-                        onChange={(e) => setCarKilometersUpdated(e.target.value)}
-                        value={carKilometersUpdated}
-                    />
-                </Form.Group>
+                <FormClient
+                    carModel={carModel}
+                    setCarModel={setCarModel}
+                    carPatent={carPatent}
+                    setCarPatent={setCarPatent}
+                    carKilometers={carKilometers}
+                    setCarKilometers={setCarKilometers}
+                />
                 <Form.Group className="mb-3" controlId="formBasicRepair">
                     <Form.Label>Reparacion</Form.Label>
                     <Form.Control
@@ -155,12 +171,16 @@ function NewService(props) {
                         type="number"
                         onChange={(e) => setAmount(e.target.value)}
                         value={amount}
+                        onWheel={(e) => e.target.blur()}
                     />
                 </Form.Group>
             </Form>
             <div className="button-container">
                 <Button variant="outline-primary" onClick={goBack}>
                     Volver
+                </Button>
+                <Button variant="danger" onClick={() => remove(params.id)}>
+                    Eliminar
                 </Button>
                 <Button variant="primary" onClick={submit}>
                     Guardar
