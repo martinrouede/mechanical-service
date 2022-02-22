b9cedac104a07630285d5882f5cfced2f07306b2
Martin Rouede <martinrouede1@gmail.com>
2022-02-22 16:46:18 -0300

    se agrega etl para pasar la data a la db

---
+++ src/etl.js
@@ -0,0 +1,52 @@
+const xlsx = require("xlsx");
+const path = require('path');
+const fs = require('fs');
+const axios = require('axios')
+
+const directoryPath = path.join(__dirname, './POR-CLIENTE/');
+
+fs.readdir(directoryPath, async (err, files) => {
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    }
+
+    files.forEach(async (file) => {
+        const aResult = {};
+        aResult.services = [];
+
+        const spreadsheet = xlsx.readFile(`/home/martinrouede/Documents/Proyectos-Personales/mechanical-service/src/POR-CLIENTE/${file}`);
+
+        let data = xlsx.utils.sheet_to_json(spreadsheet.Sheets[spreadsheet.SheetNames[0]]);
+        data = data.slice(0, data.length - 1);
+
+        data.forEach(async (res) => {
+            try {
+                let date = '';
+
+                if (res.FECHA) {
+                    let dateSplit = res.FECHA.split('.');
+
+                    dateSplit[0] = dateSplit[0].length === 1 ? 0 + dateSplit[0] : dateSplit[0];
+                    dateSplit[1] = dateSplit[1].length === 1 ? 0 + dateSplit[1] : dateSplit[1];
+
+                    dateSplit = dateSplit.reverse();
+
+                    date = dateSplit.join('-');
+                }
+
+                const service = {};
+                service.date = date;
+                service.clientName = res.CLIENTE.toUpperCase();
+                service.carModel = res['MARCA DE AUTO'] ? res['MARCA DE AUTO'].toUpperCase() : '';
+                service.carPatent = res.PATENTE ? res.PATENTE.toUpperCase() : '';
+                service.carKilometers = res.KILOMETRAJE ? res.KILOMETRAJE : 0;
+                service.amount = res.MONTO ? res.MONTO : 0;
+                service.repair = res['REPARACIÓN'] ? res['REPARACIÓN'].toUpperCase() : '';
+
+                await axios.post(`http://localhost:3000/services/`, service);
+            } catch (error) {
+                console.log(error);
+            }
+        })
+    });
+})
